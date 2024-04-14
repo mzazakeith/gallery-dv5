934a479cc1c91c98ebbe3ad8cdb8ff970c2f9437
mzazakeith <mzazakeith@gmail.com>
2024-04-14 20:57:45 +0300

    initial setup

--- _config.js
+++ _config.js
@@ -1,9 +1,10 @@
-var config = {}
+require("dotenv").config();
+let config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: `mongodb+srv:/${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.xaakrlf.mongodb.net/darkroom?retryWrites=true&w=majority`,
+    development: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.xaakrlf.mongodb.net/darkroom-dev?retryWrites=true&w=majority`,
+    test: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.xaakrlf.mongodb.net/darkroom-test?retryWrites=true&w=majority`,
 }
 module.exports = config;
