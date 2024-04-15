d8fdb46b8dda25bdf73c1c74d28b57b4a70b08bc
mzazakeith <mzazakeith@gmail.com>
2024-04-15 20:49:36 +0300

    db

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // connecting the database
 
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
+const MONGODB_URI = config.mongoURI[app.settings.env]
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
     if (err) {
         console.log(err)
@@ -46,7 +46,7 @@ app.use('/image', image);
 
 
 
- 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
