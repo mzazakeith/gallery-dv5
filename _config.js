5d3d938dc55ca08b80c653120dfce7e5423b41ff
mzazakeith <mzazakeith@gmail.com>
2024-04-15 21:02:25 +0300

    cr

--- _config.js
+++ _config.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 let config = {}
 
+let DB_USERNAME='deploy-user'
+let DB_PASSWORD='QviknVGYr5tURxTt'
+
 // Update to have your correct username and password
 config.mongoURI = {
-    production: `mongodb+srv:/${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.xaakrlf.mongodb.net/darkroom?retryWrites=true&w=majority`,
-    development: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.xaakrlf.mongodb.net/darkroom-dev?retryWrites=true&w=majority`,
-    test: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.xaakrlf.mongodb.net/darkroom-test?retryWrites=true&w=majority`,
+    production: `mongodb+srv:/${DB_USERNAME}:${DB_PASSWORD}@cluster0.xaakrlf.mongodb.net/darkroom?retryWrites=true&w=majority`,
+    development: `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.xaakrlf.mongodb.net/darkroom-dev?retryWrites=true&w=majority`,
+    test: `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.xaakrlf.mongodb.net/darkroom-test?retryWrites=true&w=majority`,
 }
 module.exports = config;
